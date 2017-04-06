fbb1931cf9cab82b3fe623efbeb5e5117f0fc75b
lanchongyizu <lanchongyizu@gmail.com>
2017-04-05 23:39:15 -0400

    Node Simulator v0.0.2
    1) change Node Simulator to a service from utility
    2) add model, controller, service and swagger config file for node

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 'use strict';
 
-var nodeManager = require('./lib/node-manager.js'),
-    taskManager = require('./lib/task-manager.js'),
+var express = require('express'),
+    http = require('http'),
+    swaggerTools = require('swagger-tools'),
+    config = require('./config.js'),
+    redisClient = require('./lib/redis-client'),
+    waterlineService = require('./lib/api/services/waterline-service'),
     logger = require('./lib/logger.js');
 
-taskManager.start()
-.then(function() {
-    logger.info("Task Manager started");
-    return nodeManager.start();
-})
-.then(function() {
-    logger.info("Node Manager started");
-})
-.catch(function(err) {
-    logger.error(err);
-    process.exit(-1);
+var app = express();
+
+var swaggerDoc = require('./lib/api/swagger.json');
+var swaggerOptions = {
+    controllers: './lib/api/controllers',
+    userStubs: process.env.NODE_ENV === 'development' ? true : false
+};
+var servicePort = config.servicePort || 9000;
+swaggerTools.initializeMiddleware(swaggerDoc, function(middleware) {
+    app.use(middleware.swaggerMetadata());
+    app.use(middleware.swaggerValidator({
+        validateResponse: true
+    }));
+    app.use(middleware.swaggerRouter(swaggerOptions));
+    app.use(middleware.swaggerUi());
+    http.createServer(app).listen(servicePort, function() {
+        logger.info('Node Simulator Service started at Port ' + servicePort);
+    });
 });
 
+redisClient.start();
+waterlineService.start();
+process.on('SIGINT', function() {
+    redisClient.stop();
+    waterlineService.stop();
+    process.exit(1);
+});
